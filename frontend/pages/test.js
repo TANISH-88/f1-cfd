a8b5e4c8669c5345190abd44707d7b10503d18f6
Tanish <tanishpalsingh5095@gmail.com>
2026-04-14 19:53:05 +0530

    Remove pages directory, use App Router only

--- frontend/pages/test.js
+++
@@ -1,9 +0,0 @@
-export default function Test() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>F1 CFD Test Page</h1>
-      <p>If you can see this, the deployment is working!</p>
-      <p>Backend URL: https://f1-cfd.onrender.com</p>
-    </div>
-  );
-}
